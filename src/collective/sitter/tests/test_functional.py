5837546d70a1dc1cc6956eddb8293cc419373f55
Thomas Lotze <thomas@thomas-lotze.net>
2021-03-14 22:06:22 +0100

    move some integration-related tests out of this package
    This leaves no more skipped tests.

--- src/collective/sitter/tests/test_functional.py
+++ src/collective/sitter/tests/test_functional.py
@@ -2,10 +2,8 @@ from ..testing import SITTER_FUNCTIONAL_TESTING
 from ..testing import SITTER_SELENIUM_TESTING
 from ..testing import TestCase
 from plone import api
-from plone.app.testing import setRoles
 from plone.app.testing import SITE_OWNER_NAME
 from plone.app.testing import SITE_OWNER_PASSWORD
-from plone.app.testing import TEST_USER_ID
 from plone.app.testing import TEST_USER_NAME
 from plone.app.testing import TEST_USER_PASSWORD
 from selenium import webdriver
@@ -13,7 +11,6 @@ from selenium.common.exceptions import NoSuchElementException
 
 import os
 import transaction
-import unittest
 
 
 SEND_SUCCESSFULLY_MSG = 'Eine E-Mail an den Babysitter wurde erfolgreich versendet'
@@ -63,34 +60,6 @@ class BaseTestClass(TestCase):
         sitter_07.nickname = 'Äh üö'
         transaction.commit()
         self.login_site_owner()
-
-
-@unittest.skip('')
-class TestRedirection(BaseTestClass):
-    def _login_using_page(self):
-        url = f'{self.portal_url}/{self.sitter_folder_name}/login'
-        self.browser.open(url)
-        username = TEST_USER_NAME
-        pswd = TEST_USER_PASSWORD
-        form = self.browser.getForm('login_form')
-        form.getControl(name='__ac_name').value = username
-        form.getControl(name='__ac_password').value = pswd
-        btn = form.getControl(name='submit')
-        print(btn)
-        btn.click()
-
-    def test_redirectionToAcceptAGBPage(self):
-        self._login_using_page()
-        self.assertIn('signupview', self.browser.url)
-        self.assertIn('Accept', self.browser.contents)
-        self.assertIn('Decline', self.browser.contents)
-
-    def test_noRedirectionAgain(self):
-        self._login_using_page()
-        self.browser.getControl(name='form.button.Accept').click()
-        self._login_using_page()
-        self.assertIn(self.sitter_folder_name, self.browser.url)
-        self.assertIn('Babysitter Liste filtern', self.browser.contents)
 
 
 class BaseSeleniumTestClass(BaseTestClass):
@@ -210,37 +179,6 @@ class TestRemovePortalMessage(BaseSeleniumTestClass):
         self.assertNotIn('Item state changed.', self.driver.page_source)
         self.assertNotIn('Artikelstatus geändert.', self.driver.page_source)
         self.assertIn('Kontaktformular', self.driver.page_source)
-
-
-class TestPortalActions(BaseTestClass):
-
-    layer = SITTER_FUNCTIONAL_TESTING
-
-    def test_manager_siteActionVisible(self):
-        self.login_test_user()
-        setRoles(self.portal, TEST_USER_ID, ['Member', 'Manager'])
-        transaction.commit()
-
-        url = f'{self.portal_url}/{self.sitter_folder_name}'
-        self.browser.open(url)
-        self.assertIn('portal-personaltools', self.browser.contents)
-        self.assertIn('Log out', self.browser.contents)
-        self.assertIn('test_user_1', self.browser.contents)
-        self.assertIn('logout', self.browser.contents)
-
-    @unittest.skip('')
-    def test_testUser_siteActionNotVisible(self):
-        self.login_test_user()
-        url = f'{self.portal_url}/{self.sitter_folder_name}'
-        self.browser.open(url)
-        self.assertNotIn('test_user_1', self.browser.contents)
-        self.assertIn('logout', self.browser.contents)
-
-    def test_anonymous_noSiteActionNoLogout(self):
-        self.logout()
-        url = f'{self.portal_url}/{self.sitter_folder_name}'
-        self.browser.open(url)
-        self.assertNotIn('logout', self.browser.contents)
 
 
 class TestReferer(BaseSeleniumTestClass):
