b5a9216358018fc964736d8b8b0f21f022b49e85
Thomas Lotze <lotze@starzel.de>
2022-07-31 17:54:31 +0200

    configure faceted navigation via an ObjectAddedEvent handler
    Doing it via an add-on setup handler used to assume that the sitter
    folder would be added upon setup, which may be useful for specific
    deployments but not for an independently released add-on.

--- src/collective/sitter/content/sitterfolder.py
+++ src/collective/sitter/content/sitterfolder.py
@@ -1,14 +1,26 @@
 from .. import MessageFactory as _
+from eea.facetednavigation.interfaces import IDisableSmartFacets
+from eea.facetednavigation.interfaces import IFacetedNavigable
+from eea.facetednavigation.interfaces import IHidePloneRightColumn
+from eea.facetednavigation.layout.events import faceted_enabled
+from eea.facetednavigation.layout.interfaces import IFacetedLayout
 from plone.app.z3cform.widget import RelatedItemsFieldWidget
 from plone.autoform import directives
 from plone.dexterity.content import Container
 from plone.supermodel import model
+from Products.GenericSetup.context import SnapshotImportContext
+from Products.GenericSetup.interfaces import IBody
 from z3c.relationfield.schema import RelationChoice
 from z3c.relationfield.schema import RelationList
 from zope import schema
+from zope.component import adapter
+from zope.component import queryMultiAdapter
+from zope.interface import alsoProvides
 from zope.interface import implementer
+from zope.lifecycleevent import IObjectAddedEvent
 
 import logging
+import os
 
 
 logger = logging.getLogger(__name__)
@@ -108,3 +120,30 @@ class ISitterFolder(model.Schema):
 @implementer(ISitterFolder)
 class SitterFolder(Container):
     pass
+
+
+@adapter(ISitterFolder, IObjectAddedEvent)
+def configure_facetednavigation(sitterfolder, event=None):
+    config_file_name = 'facetednavigation.xml'
+    layout_id = 'faceted-sitter'
+
+    alsoProvides(sitterfolder, IFacetedNavigable)
+    alsoProvides(sitterfolder, IDisableSmartFacets)
+    alsoProvides(sitterfolder, IHidePloneRightColumn)
+    faceted_enabled(sitterfolder, None)
+
+    logger.info(
+        f'Loading configuration {config_file_name} for faceted view '
+        f'on {"/".join(sitterfolder.getPhysicalPath())}'
+    )
+    import_file_path = os.path.join(os.path.dirname(__file__), config_file_name)
+    with open(import_file_path) as import_file:
+        xml = import_file.read()
+
+        # XXX quick fix to limit search, there should be a less hacky way to do this
+        xml = xml.replace('SITTERFOLDER', f'/{sitterfolder.getId()}')
+
+        environ = SnapshotImportContext(sitterfolder, 'utf-8')
+        importer = queryMultiAdapter((sitterfolder, environ), IBody)
+        importer.body = xml
+    IFacetedLayout(sitterfolder).update_layout(layout_id)
