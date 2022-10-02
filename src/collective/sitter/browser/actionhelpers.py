cefac9dce63e76917a08dbd6d4448183f12feed2
Thomas Lotze <lotze@starzel.de>
2022-10-02 18:44:49 +0200

    re plan.io #32434: add user action for rejecting usage agreement
    This is helpful in testing processes, which is why it's being added right now.
    Still need to make sure of the implications, such as retracting any sitter
    entry and resetting the registration process.

--- src/collective/sitter/browser/actionhelpers.py
+++ src/collective/sitter/browser/actionhelpers.py
@@ -1,4 +1,5 @@
 from ..sitterstate import ISitterState
+from plone import api
 from Products.Five.browser import BrowserView
 
 
@@ -14,3 +15,12 @@ class ActionHelpers(BrowserView):
         sitter = self.get_sitter()
         if sitter is not None:
             return sitter.getObject().absolute_url()
+
+    def accepted_sitter_agreement(self):
+        sitter_state = ISitterState(self.context)
+        return sitter_state.has_accepted()
+
+    def reject_sitter_agreement(self):
+        user = api.user.get_current()
+        user.setMemberProperties(mapping={'accepted_sitter_agreement': False})
+        self.request.response.redirect(self.sitter_path())
