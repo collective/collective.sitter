57494dd00460158f468ca521f55f3cc70ae62c85
Jonathan Hofmann <hofman5x@hrz.uni-marburg.de>
2022-11-01 08:56:13 +0100

    cleanup sitteraccount and macro
    move macro back into sitterview.pt
    remove sittermanager from sitteraccount view

--- src/collective/sitter/browser/macros.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from Products.Five import BrowserView
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from zope.interface import implementer
-
-
-class SitterMacros(BrowserView):
-    pass
--- src/collective/sitter/browser/sitteraccount.py
+++ src/collective/sitter/browser/sitteraccount.py
@@ -14,12 +14,6 @@ logger = logging.getLogger(__name__)
 class SitterAccountView(BrowserView):
 
     @property
-    def is_manager(self):
-        return api.user.has_permission(
-            'collective.sitter: Manage sitters', obj=self.context
-        )
-
-    @property
     def sitter_state(self):
         return ISitterState(self.context)
 
@@ -33,17 +27,11 @@ class SitterAccountView(BrowserView):
 
     @property
     def account_intro_text(self):
-        if (self.is_manager):
-            return api.portal.get_registry_record('sitter.sitteraccount_intro_text_manager')
-        else:
-            return api.portal.get_registry_record('sitter.sitteraccount_intro_text_sitter')
+        return api.portal.get_registry_record('sitter.sitteraccount_intro_text_sitter')
 
     @property
-    def get_fqa(self):
-        if (self.is_manager):
-            return api.portal.get_registry_record('sitter.sitteraccount_faq_manager')
-        else:
-            return api.portal.get_registry_record('sitter.sitteraccount_faq_sitter')
+    def get_faq(self):
+        return api.portal.get_registry_record('sitter.sitteraccount_faq_sitter')
 
     def get_current_registration_step(self):
         return self.sitter_state.get_current_step()
@@ -74,33 +62,3 @@ class SitterAccountView(BrowserView):
         auth_token = createToken()
         auth = f'_authenticator={auth_token}'
         return f'{sitter.getURL()}/content_status_modify?workflow_action=recycle&{auth}'
-
-    @property
-    def info_text_url(self):
-        try:
-            info_text_obj = self.sitter_folder.info_text
-            return self._get_link_for(info_text_obj)
-        except AttributeError:
-            return None
-
-    @property
-    def info_text_logged_in_url(self):
-        try:
-            info_text_logged_in_obj = self.sitter_folder.info_text_logged_in
-            return self._get_link_for(info_text_logged_in_obj)
-        except AttributeError:
-            return None
-
-    @property
-    def agreement_url(self):
-        try:
-            agreement_obj = self.sitter_folder.agreement
-            return self._get_link_for(agreement_obj)
-        except AttributeError:
-            return None
-
-    def _get_link_for(self, relation_obj):
-        if relation_obj:
-            path = relation_obj.to_path
-            if path:
-                return f'{path}/edit'
