12edcf325047791c1a6b3838152822a07950ad9d
Thomas Lotze <lotze@starzel.de>
2022-11-23 18:09:49 +0100

    redirect after login, either to account or sittermanager page

---
+++ src/collective/sitter/browser/postlogin.py
@@ -0,0 +1,30 @@
+from ..content.sitterfolder import ISitterFolder
+from ..sitterstate import ISitterState
+from plone import api
+from Products.CMFPlone.interfaces import IRedirectAfterLogin
+from zope.interface import implementer
+
+
+@implementer(IRedirectAfterLogin)
+def redirect(context, request):
+    sitterstate = ISitterState(context)
+    sitter_folder = sitterstate.get_sitter_folder()
+    if sitter_folder is None:
+        for sitter_folder in api.portal.get().objectValues():
+            if ISitterFolder.providedBy(sitter_folder):
+                break
+        else:
+            return
+
+    def adapter(*args):
+        is_manager = api.user.has_permission(
+            'collective.sitter.ManageSitters', obj=sitter_folder
+        )
+        target = (
+            f'{sitter_folder.absolute_url()}/sittermanager'
+            if is_manager
+            else f'{sitter_folder.absolute_url()}/account'
+        )
+        return target
+
+    return adapter
